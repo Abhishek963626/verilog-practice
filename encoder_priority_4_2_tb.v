0672a9acceb26c1e66a5a7e7751593da1955cd77
Abhishek963626 <sahkumarabhishek231@gmail.com>
2026-08-31 14:52:45 +0530

    added encoder_priority_4_2 testbench

---
+++ encoder_priority_4_2_tb.v
@@ -0,0 +1,48 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 24.08.2026 13:39:06
+// Design Name: 
+// Module Name: encoder_priority_4_2_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module encoder_priority_4_2_tb(
+
+    );
+    reg [3:0]din;
+    wire [1:0]y;
+    
+    encoder_priority_4_2 dut(din,y);
+    
+    initial begin
+    {din}=0;
+    end
+    initial begin
+    din=4'b0001;
+    #10;
+    
+    din=4'b0010;
+    #10; 
+    
+    din=4'b0100;
+    #10;
+    
+    din=4'b1000;
+    #10;
+    end
+    
+endmodule
